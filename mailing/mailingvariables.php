5feeca35ba32c1d9bb0a1eab448c64fe9c19f8e8
GebinTom <126493227+Gtom09@users.noreply.github.com>
2025-01-15 13:27:40 +0530

    Update mailingvariables.php

--- mailing/mailingvariables.php
+++ mailing/mailingvariables.php
@@ -2,6 +2,6 @@
     $mail_host = "smtp.gmail.com";
     $mail_port = "587";
     $mail_sender_email = "khankahan979@gmail.com"; //sender
-    $mail_sender_password = "Gebintom@2002"; //sender
+    $mail_sender_password = "aiwe mhzz sgqs fpnc"; //sender
     $mail_sender_name = "Khan Kahan";
 ?>
