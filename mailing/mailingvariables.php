b4dc3ccfe4d1459ee174e271e277d30f41594da5
GebinTom <126493227+Gtom09@users.noreply.github.com>
2025-01-15 13:11:13 +0530

    Update mailingvariables.php

--- mailing/mailingvariables.php
+++ mailing/mailingvariables.php
@@ -3,5 +3,5 @@
     $mail_port = "587";
     $mail_sender_email = "khankahan979@gmail.com"; //sender
     $mail_sender_password = "Gebintom@2002"; //sender
-    $mail_sender_name = "SERVODOX";
+    $mail_sender_name = "Khan Kahan";
 ?>
